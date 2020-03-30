ea349850948a9ee28df920fd2fbd1db58835b37e
Kiddiflame <kiddiflame@gmail.com>
2020-03-30 15:00:26 +0000

    Add files via upload

--- eventBackend/index.js
+++ eventBackend/index.js
@@ -13,8 +13,8 @@ const cors = require('cors');
 
 const apiPath = '/api/';
 const version = 'v1';
-
 var mongoURI = 'mongodb://localhost:27017/eventbackend';
+
 var port = process.env.PORT || 3000;
 
 // Connect to MongoDB
@@ -37,26 +37,14 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Enable Auth for all of the following endpoints:
-app.use(basicAuth(((req, res, next) => {
+function authorize(user, password){
 
-        const auth = {
-        users: sha256( {'admin':'secret'} ) }
-      
-        // parse login and password from headers
-        const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
-        const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
-      
-        // Verify login and password are set and correct
-        if (login && password && login === auth.login && password === auth.password) {
-          // Access granted...
-          return next()
-        }
-      
-        // Access denied...
-        res.set('WWW-Authenticate', 'Basic realm="401"') // 
-        res.status(401).send('Authentication required.') // 
-      
-})));
+    if((user === 'admin' && sha256(password) == '2bb80d537b1da3e38bd30361aa855686bde0eacd7162fef6a25fe97bf527a25b')){
+        return true
+    }
+    return false
+}
+
 
 //Event endpoints
 
